d0f5f4e0079ecbb0b2a3d9b2dd4405bd20828e23
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-25 08:10:13 -0300

    Update dupla.js

--- Exercício01/dupla.js
+++ Exercício01/dupla.js
@@ -1,1 +1,25 @@
+var aluno1 = {
+    nome: "Thalita de Jesus Conceição",
+    curso: "Desenvolvimento de Sistemas",
+    matricula: 1526.1464,
+    ativo: true
+}
 
+var aluno2 = {
+    nome: "Thais Mirelle Araújo dos Santos",
+    curso: "Desenvolvimento de Sistemas",
+    matricula: 1526.1515,
+    ativo: true
+}
+
+//Primeiro aluno
+console.log(aluno1.nome);
+console.log(aluno1.curso);
+console.log(aluno1.matricula);
+console.log(aluno1.ativo);
+
+//Segundo Aluno
+console.log(aluno2.nome);
+console.log(aluno2.curso);
+console.log(aluno2.matricula);
+console.log(aluno2.ativo);
