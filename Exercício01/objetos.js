e1c3024d550f844ec2294b2bf55752a27cacb47d
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-25 08:09:14 -0300

    Add files via upload

---
+++ Exercício01/objetos.js
@@ -0,0 +1,21 @@
+//Objetos
+var aluno1 = {
+    matricula: 7627364,
+    nome: "Danilo Ferreira",
+    cursos: "Bootcamp Front End",
+    ativo: true
+};
+
+console.log(aluno1.nome);
+
+aluno1.dataNascimento = "03/10/1985";
+console.log(aluno1.dataNascimento);
+
+delete aluno1.dataNascimento;
+
+//Undefined e null
+var x;
+var y = null;
+
+// Arrays
+var frutas = ['Banana', 'Laranja', 'Maçã'];
---
+++ Exercício01/primitivos.js
@@ -0,0 +1,4 @@
+//Tipos de dados
+var vNumber = 17.5;
+var vString = "Olá";
+var vBoolean = true;
