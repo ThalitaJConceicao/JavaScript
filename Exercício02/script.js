13083f4206538d31a3662b7f5697bda8d1caae3f
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-27 07:46:36 -0300

    Create script.js

---
+++ Exercício02/script.js
@@ -0,0 +1,15 @@
+var m = 'Olá';
+imprime();
+
+function imprime() {
+    var nome= 'Danilo';
+    console.log(m);
+    console.log(nome);
+    imprimeOla2();
+
+    function imprimeOla2() {
+        console.log(m);
+        console.log(nome);
+    }
+
+}
